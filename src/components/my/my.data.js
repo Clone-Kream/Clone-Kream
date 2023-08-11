cc9a478d2f6081ec439ba88adbc78bfa16d46ed1
Sim <0doogadooga@gmail.com>
2023-08-11 12:43:59 +0900

    feat : 관심 상품, 최근 본 상품 제외

--- src/components/my/my.data.js
+++ src/components/my/my.data.js
@@ -18,8 +18,8 @@ export const user = {
 export const shoppingInfo = [
   "구매 내역",
   "판매 내역",
-  "관심 상품",
-  "최근 본 상품",
+  // "관심 상품",
+  // "최근 본 상품",
 ];
 
 // export const myInfo = ["금액 관리", "프로필 관리"];
